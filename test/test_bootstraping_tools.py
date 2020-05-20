6db0db64dd79ddac00b963363f0fbe7100678224
Fernando <fernando.alvarez@islas.org.mx>
2020-05-20 15:52:20 -0700

    agrega prueba de la funcion season_from_date

--- test/test_bootstraping_tools.py
+++ test/test_bootstraping_tools.py
@@ -1,4 +1,7 @@
-import bootstraping_tools as bt
+from bootstraping_tools import *
 
 def test_seasons_from_date():
-    assert
+    input_date=pd.DataFrame(["13/May/2019","16/May/2018","23/Abr/2020","25/Abr/2018"], columns =['Fecha'])
+    expected=np.array(["2019","2018","2020","2018"])
+    obtained=seasons_from_date(input_date)
+    np.testing.assert_array_equal(obtained,expected)
