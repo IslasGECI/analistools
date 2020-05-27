8e8b0a9c6e71d004ac0cb7b20c1723827d7c9529
Fernando <fernando.alvarez@islas.org.mx>
2020-05-27 14:34:58 -0700

    agrega paquete cli_paths al setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="bootstraping_tools",
     version="0.1.0",
-    packages=["bootstraping_tools"],
+    packages=["bootstraping_tools","cli_paths"],
     author="Fernando Alvarez",
     python_requires='>=3.6'
 )
